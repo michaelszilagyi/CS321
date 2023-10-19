c735227db2f307a36b968ad861e3c1031dce1fdf
hiba-awan <hiba.m.awan@gmail.com>
2023-10-19 15:04:00 -0400

    Added setters and getters

--- src/Declaration.java
+++ src/Declaration.java
@@ -1,3 +1,56 @@
 public class Declaration {
+    public String date;
+    public String name;
+    public String email;
+    public int durationOfSupport;
+    public int applicantNumber;
+    public String immigrantName;
+    public int alienNumber;
+    public Boolean isExpired;
+    public int declarationID;
+
+    public Declaration(){
+        
+    }
+
+    public Declaration(String date, String name, String email, int durationOfSupport, int applicantNumber, String immigrantName, int alienNumber, Boolean isExpired, int declarationID){
+        
+    }
+
+    public String getDate(){
+        return null;
+    }
+
+    public void setDate(String date){
+        this.date = null;
+    }
+
+    public String getName(){
+        return null;
+    }
+
+    public void setName(String name){
+        this.name = null;
+    }
+
+    public String getEmail(){
+        return null;
+    }
+
+    public void setEmail(String email){
+        this.email = null;
+    }
+
+    public int getDuration(){
+        return null;
+    }
+
+    public void setDuration(int duration){
+        this.durationOfSupport = null;
+    }
+
+    public int getApplicantNumber(){
+        return null;
+    }
 
 }
