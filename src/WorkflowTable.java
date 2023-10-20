b334182b7c4356bc44f53f8793cfbd40fe15d8dd
hiba-awan <hiba.m.awan@gmail.com>
2023-10-20 08:54:06 -0400

    Added Workflow methods

--- src/WorkflowTable.java
+++ src/WorkflowTable.java
@@ -1,3 +1,21 @@
-public class WorkflowTable {
+import java.util.*;
 
+public class WorkflowTable {
+    private static Queue<Tuple> tasks = new LinkedList<Tuple>();
+
+    public WorkflowTable(){
+
+    }
+
+    public int getNextItem(String step){
+        return -1;
+    }
+
+    public static void addTask(int id, String step){
+
+    }
+
+    public static void removeTask(){
+
+    }
 }
