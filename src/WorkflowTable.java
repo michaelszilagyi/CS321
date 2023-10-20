a1ffe3b8bfa5a5c36e67abde2f28701561e1b6ec
Miguel <dynamite.email@protonmail.com>
2023-10-20 14:19:29 -0400

    finished workflow tests. they are built with the assumption that state can persist between tests, though idk if junit allows that.

--- src/WorkflowTable.java
+++ src/WorkflowTable.java
@@ -6,22 +6,23 @@ public class WorkflowTable {
         REVIEW,
         APPROVAL
     }
-    public static Queue<Task> tasks = new LinkedList<Task>();
+    public static Queue<Task> tasks;
 
+    /* work */
     public WorkflowTable(){
-
+        tasks = new LinkedList<Task>();
     }
 
-    public static Task getTask(Step step){
+    public Task getTask(Step step){
         return null;
     }
 
-    public static void addTask(Task t){
+    public void addTask(Task t){
 
     }
 
     /* This should remove the task with the given id, and then return it */
-    public static Task removeTask(int id){
+    public Task removeTask(int id){
         return null;
     }
 }
--- src/WorkflowTest.java
+++ src/WorkflowTest.java
@@ -4,35 +4,50 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
+/* Test the methods in WorkflowTable.
+ * Tests must be run from top to bottom, as subsequent tests use
+ * persistent state from previous ones.
+ */
 public class WorkflowTest {
-    
+    int id = 1;
+    WorkflowTable table;
     @Test
     void testConstructor(){
+        table = new WorkflowTable();
         Queue<Task> temp = new LinkedList<Task>();
-        Task task = new Task(1, "Review");
+
+        Task task = new Task(++id, WorkflowTable.Step.REVIEW);
         temp.add(task);
-        WorkflowTable.addTask(task);
+        table.addTask(task);
         assertEquals(temp, WorkflowTable.tasks);
     }
 
+    /* Tests getTask and addTask.
+     * Assumes table was properly initialized in previous test
+     */
     @Test
-    void testGetTask(){
-        var test_task = new Task(1, "")
+    void testAddAndGet(){
+        var task1 = new Task(++id, WorkflowTable.Step.REVIEW);
+        var task2 = new Task(++id, WorkflowTable.Step.APPROVAL);
+        var task3 = new Task(++id, WorkflowTable.Step.REVIEW);
 
-        var declID = WorkflowTable.getTask(WorkflowTable.Step.REVIEW);
-        assertEquals(1, declID.id);
+        table.addTask(task1);
+        table.addTask(task2);
+        table.addTask(task3);
+
+        var dec1 = table.getTask(WorkflowTable.Step.REVIEW);
+        var dec2 = table.getTask(WorkflowTable.Step.APPROVAL);
+        assertEquals(task3.id, dec1.id);
+        assertEquals(task2.id, dec2.id);
     }
 
-    @Test
-    void testAddTask() {
-
-    }
-
+    /* Tests the removeTask method.
+     * Assumes table has been initialized.
+     */
     @Test
     void testRemoveTask(){
-        int id = 12;
-        Task task = new Task(id, WorkflowTable.Step.REVIEW);
-        Task removedTask = WorkflowTable.removeTask(id);
+        Task task = new Task(++id, WorkflowTable.Step.REVIEW);
+        Task removedTask = table.removeTask(id);
         assertEquals(task, removedTask);
     }
 
