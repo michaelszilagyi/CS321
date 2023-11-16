43f9645e8e3b1894248c8ca19acffe6560e77827
michaelszilagyi <michaelszilly@gmail.com>
2023-11-16 10:34:18 -0500

    Changes to workflow

--- src/WorkflowTable.java
+++ src/WorkflowTable.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.HashMap;
+
 
 public class WorkflowTable {
     public enum Step {
@@ -6,23 +8,36 @@ public class WorkflowTable {
         REVIEW,
         APPROVAL
     }
-    public static Queue<Task> tasks;
+    private static LinkedHashMap<Integer,Step> WF;
 
     /* work */
     public WorkflowTable(){
-        tasks = new LinkedList<Task>();
+        WF = new LinkedHashMap<Integer,Step>();
     }
 
-    public Task getTask(Step step){
+    public Integer getTask(Step step){
+        Set<Integer> keys = WF.keySet();
+        for(Integer key : keys){
+            if(WF.get(key) == step){
+                //since they are kept in the order they are inserted, this will find the first
+                //task of a given step
+
+                //return the unique Declaration ID of this Task, which can then be used to find the Declaration
+                //object in the fake database
+                return key;
+            }
+        }
+        //return null if there are no Tasks for the given Step
         return null;
     }
 
-    public void addTask(Task t){
-
+    public void addTask(Integer DeclarationID, Step step){
+        //puts the given information at the end of the LinkedHashMap
+        WF.put(DeclarationID,step);
     }
 
-    /* This should remove the task with the given id, and then return it */
-    public Task removeTask(int id){
-        return null;
+    /* This should remove the task with the given id*/
+    public void removeTask(Integer DeclarationID){
+        WF.remove(DeclarationID);
     }
 }
